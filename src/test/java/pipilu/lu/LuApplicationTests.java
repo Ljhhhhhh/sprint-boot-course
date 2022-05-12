38d24290d20888f5accb79ed719926144b91f772
卢洁辉 <lujh@olymtech.com>
2022-05-12 22:27:01 +0800

    初始化

---
+++ src/test/java/pipilu/lu/LuApplicationTests.java
@@ -0,0 +1,13 @@
+package pipilu.lu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
