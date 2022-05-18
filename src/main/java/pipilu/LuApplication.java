9d0bf83911f1050e39422f2f81bc6a28117bd1f3
卢洁辉 <lujh@olymtech.com>
2022-05-18 21:58:56 +0800

    18-

--- src/main/java/pipilu/LuApplication.java
+++ src/main/java/pipilu/LuApplication.java
@@ -1,13 +1,13 @@
-package pipilu.lu;
+package pipilu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("pipilu.dao")
 public class LuApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LuApplication.class, args);
 	}
-
 }
